416bd9efe900f81f0d298515581eddd305c5f244
Divyanshu <kdivyanshu@hotmail.com>
2021-06-21 20:11:17 +0530

    effect change

--- particle/particle.js
+++ particle/particle.js
@@ -1,15 +1,53 @@
+var particlesQuantity = 50;
+
+var positionX = new Array(particlesQuantity);
+var positionY = new Array(particlesQuantity);
+var velocityX = new Array(particlesQuantity).fill(0);
+var velocityY = new Array(particlesQuantity).fill(0);
 function setup(){
   createCanvas(window.innerWidth, window.innerHeight);
-
+  stroke(random(0,255), random(0,255), 255);
+  strokeWeight(3);
+	
+	for (var particle = 1; particle < particlesQuantity; particle++) {
+		positionX[particle] = random(0, width);
+		positionY[particle] = random(0, height);
+	}
+	
+	positionX[0] = 0;
+	positionY[0] = 0;
 }
 function draw(){
-   background("#00000055");
-   fill(random(200,255),random(200,255),random(200,255),5);
-  stroke(255,255,random(0,255),30);
- circle(mouseX,mouseY,random(20,100));
- fill(random(0,255),random(0,255),random(0,255),255);
- ellipse(random(0,width),random(0,width),random(0,10),random(0,10));
- rect(random(0,width),random(0,width),random(0,10),random(0,10));
- ellipse(random(0,width),random(0,width),random(0,10),random(0,20));
- rect(random(0,width),random(0,width),random(0,10),random(0,20));
+  background(0);	
+	velocityX[0] = velocityX[0] * 0.5 + (mouseX - positionX[0]) * 0.1;
+	velocityY[0] = velocityY[0] * 0.5 + (mouseY - positionY[0]) * 0.1;
+	
+	positionX[0] += velocityX[0];
+	positionY[0] += velocityY[0];
+	
+	for (var particle = 1; particle < particlesQuantity; particle++) {
+		var whatever = 1024 / (sq(positionX[0] - positionX[particle]) + sq(positionY[0] - positionY[particle]));
+		
+		velocityX[particle] = velocityX[particle] * 0.95 + (velocityX[0] - velocityX[particle]) * whatever;
+		velocityY[particle] = velocityY[particle] * 0.95 + (velocityY[0] - velocityY[particle]) * whatever;
+		
+		positionX[particle] += velocityX[particle];
+		positionY[particle] += velocityY[particle];
+		
+		if ((positionX[particle] < 0 && velocityX[particle] < 0) || (positionX[particle] > width && velocityX[particle] > 0)) {
+			velocityX[particle] = -velocityX[particle];
+		}
+		
+		if ((positionY[particle] < 0 && velocityY[particle] < 0) || (positionY[particle] > height && velocityY[particle] > 0)) {
+			velocityY[particle] = -velocityY[particle];
+		}
+		
+		point(positionX[particle], positionY[particle]);
+	}
+}
+function mousePressed() {
+	for (var particle = 1; particle < particlesQuantity; particle++) {
+		positionX[particle] = random(0, width);
+		positionY[particle] = random(0, height);
+	}
 }
